48c338370997bf36a39c9af4073de0fe835be01d
James Nolan <jamesn8391@gmail.com>
2023-03-17 20:52:21 -0500

    changed type to embed instead of regular message and added origin in message struct

--- message.go
+++ message.go
@@ -36,9 +36,73 @@ type Message struct {
 	Message    string    `json:"message"`
 	Suggestion string    `json:"suggestion,omitempty"`
 	Time       time.Time `json:"time,omitempty"`
+	Origin 	   string	 `json:"origin" example:"from:api/from:gdc"`
 }
 
 func sendDiscordMessage(s *discordgo.Session, channelID string, msg Message) error {
-	_, err := s.ChannelMessageSend(channelID, fmt.Sprintf("**TYPE**\n%s\nMessage:%s\nSuggestion:%s", msg.Type, msg.Message, msg.Suggestion))
-	return err
+
+	embed_full := &discordgo.MessageEmbed{
+		Author:      &discordgo.MessageEmbedAuthor{Name: "Adomate Bot"},
+    	Color:       0x00ff00, // Green - should change later based on message 
+		Description: "This is a message from the api bot",
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name:   "Type:",
+				Value:  msg.Type,
+				Inline: true,
+			},
+			&discordgo.MessageEmbedField{
+				Name:   "Message: ",
+				Value:  msg.Message,
+				Inline: true,
+			},
+			&discordgo.MessageEmbedField{
+				Name:   "Suggestion: ",
+				Value:  msg.Suggestion,
+				Inline: true,
+			},
+			// &discordgo.MessageEmbedField{ --- not sure how to pass time field into rabbitmq
+			// 	Name:   "Time: ",
+			// 	Value:  msg.Time.String(),
+			// 	Inline: true,
+			// },
+
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+				Text: msg.Origin,
+		},
+	}	
+	embed_no_suggestion := &discordgo.MessageEmbed{
+		Author:      &discordgo.MessageEmbedAuthor{Name: "Adomate Bot"},
+    	Color:       0x00ff00, // Green - should change later based on message 
+		Description: "This is a message from the api bot",
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name:   "Type:",
+				Value:  msg.Type,
+				Inline: true,
+			},
+			&discordgo.MessageEmbedField{
+				Name:   "Message: ",
+				Value:  msg.Message,
+				Inline: true,
+			},
+			// &discordgo.MessageEmbedField{
+			// 	Name:   "Time: ",
+			// 	Value:  msg.Time.String(),
+			// 	Inline: true,
+			// },
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+				Text: msg.Origin,
+		},
+	}	
+
+	if msg.Suggestion == "" { //discord fills out embed with all fields even if they are empty in struct and omitempty
+		_, err := s.ChannelMessageSendEmbed(channelID, embed_no_suggestion)
+		return err
+	} else{
+		_, err := s.ChannelMessageSendEmbed(channelID, embed_full)
+		return err
+	}
 }
